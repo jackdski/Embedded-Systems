142be40718e401c3a09bab60c0105da62ce06b28
Jack Danielski <jackdski555@gmail.com>
2017-09-07 09:50:22 -0600

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -8,8 +8,12 @@
 #include "lab1.h"
 #include <stdint.h>
 
+/*
+ * Problem 5)
+ * Report size of different types
+ */
 void report_types() {
-    int char_size   =   sizeof(char);
+    int char_size = sizeof(char);
     int int_size    =   sizeof(int);
     int short_size  =   sizeof(short);
     int long_size   =   sizeof(long);
@@ -31,6 +35,9 @@ void report_types() {
     int uint32_size = sizeof(uint32_t);
 }
 
+/*
+ *
+ */
 void report_pointer_types() {
     int char_pointer = sizeof(char*);
     int int_pointer = sizeof(int*);
@@ -52,12 +59,24 @@ void report_pointer_types() {
     int uint16_pointer = sizeof(uint16_t*);
     int int32_pointer = sizeof(int32_t*);
     int uint32_pointer = sizeof(uint32_t*);
+    //return char_pointer+int_pointer+short_pointer
 }
 
-void make_square_wave(int delay) {
-    P1_DIR |= 0x01;
-    P1_OUT ^= 0x01;
-    for (int i = 0; i < delay; i++);
-    P1_OUT ^= 0x01;
+/*
+ * Problem 8)
+ * Problem 9)
+ *
+ * Set the direction to PIN 1.7 and the output of square wave pin 1.7.
+ */
+void make_square_wave() {
+    P1_DIR |= 0x80;
+    volatile int i;
+
+    P1_OUT |= 0x80;
+
+    for (i = 0; i < 14395; i++);
+    P1_OUT ^= 0x80;
+    for(i=0; i<14395; i++);
+    P1_OUT ^= 0x80;
 
 }
--- lab1.h
+++ lab1.h
@@ -9,11 +9,11 @@
 #define LAB1_H
 
 
-#define P1_IN    (*(uint32_t *)(0x40004C00))
-#define P1_OUT   (*(uint32_t *)(0x40004C02))
-#define P1_DIR   (*(uint32_t *)(0x40004C04))
-#define P1_SEL0  (*(uint32_t *)(0x40004C0A))
-#define P1_SEL1  (*(uint32_t *)(0x40004C0C))
+#define P1_IN    (*(uint8_t *)(0x40004C00))
+#define P1_OUT   (*(uint8_t *)(0x40004C02))
+#define P1_DIR   (*(uint8_t *)(0x40004C04))
+#define P1_SEL0  (*(uint8_t *)(0x40004C0A))
+#define P1_SEL1  (*(uint8_t *)(0x40004C0C))
 
 void report_types();
 
