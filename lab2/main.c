fc0433ee755f6b3c2605a8f1a99063352d06db61
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-09-21 09:21:38 -0600

    Update main.c

--- lab2/main.c
+++ lab2/main.c
@@ -12,6 +12,7 @@
 
 volatile uint16_t sysclock;
 volatile uint32_t beamBreaks = 0;
+volatile float distanceTraveled = 0;
 int part_twelve = 0;
 
 void main(void)
@@ -23,7 +24,7 @@ void main(void)
   timer_a0_config();
 
   __enable_irq();
-  
+
   // SysTick Config
   SysTick->LOAD = 0X00FFFFFF;   // Give SysTick a starting value to count down from
   SysTick->CTRL = BIT0 | BIT2;  // Enable SysTick
@@ -33,22 +34,27 @@ void main(void)
   //P1->OUT |= BIT7;
   //P1->IFG |= BIT1;
 #endif
-  
+
 #ifdef PROB3B
   sysclock = SysTick->VAL;  // STCVR starting value
   P1->OUT |= BIT7;          // Turn P1.7 high
   P1->IFG |= BIT1;          // manually create flag
 #endif
 
+#ifdef PROB12
   part_twelve = SysTick->VAL;
-  P1->IFG |= BIT5;
+  //P1->IFG |= BIT5;
   part_twelve -= SysTick->VAL;
-  
+#endif
 
   while (1){
+#ifdef PROB12
+      distanceTraveled = beamBreaks * 0.9107;
+#endif
       //P1->OUT ^= BIT0;
-      P1->IFG |= BIT5;
+      //P1->IFG |= BIT5;
       //for(i = 0; i<200000; i++);
+
   }
 
 }
