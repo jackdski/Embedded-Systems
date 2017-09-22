9b3625cbb969144d71ddc7ba26d2b6ae6cdf105d
Jack Danielski <jackdski555@gmail.com>
2017-09-21 21:21:38 -0600

    Update port.h

--- lab2/port.h
+++ lab2/port.h
@@ -2,19 +2,16 @@
  * port.h
  *
  *  Created on: Sep 7, 2017
- *      Author: amabo and Jack
+ *      Author: Avery Anderson and Jack Danielski
  */
 
 #ifndef PORT_H_
 #define PORT_H_
-//#define PROB12
 
 #include "msp.h"
 
 #define CLEAR_FLAGS (uint8_t) (~(BIT1 | BIT4 | BIT5))
 
-//void ISR_FUNCTION_PORTS();
-
 void GPIO_configure(void);
 
-#endif /* PORT_H_ */
+#endif 
