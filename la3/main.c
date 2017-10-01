671f7ec0bdc1da81e246c5459e9655a6d0101df4
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-10-01 11:58:21 -0600

    Update main.c

--- la3/main.c
+++ la3/main.c
@@ -1,1 +1,23 @@
+#include "msp.h"
+#include "uart.h"
 
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	configure_clocks();
+	configure_serial_port();
+
+	uint8_t data = 0xAA;
+	uint8_t ptr[] = {0xAA,0xBB};
+		//UART_send_byte(data);
+
+
+	while(1){
+	    UART_send_n(ptr, 1);
+	}
+}
