e94c68a0b2512458bdbb1efc4e705252c195484e
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-10-01 11:58:55 -0600

    Update uart.c

--- la3/uart.c
+++ la3/uart.c
@@ -7,21 +7,52 @@
 #include "uart.h"
 
 void configure_serial_port(){
+    //Configure UART pins, set 2-UART pins to UART mode
     P1->SEL0 |=  (BIT2 | BIT3);
     P1->SEL1 &= ~(BIT2 | BIT3);
 
-    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST;
-    EUSCI_A0->CTLW0 &= ~(EUSCI_A_CTLW0_PEN | EUSCI_A_CTLW0_MSB | 
-                         EUSCI_A_CTLW0_SEVENBIT | EUSCI_A_CTLW0_SPB);
-   // EUSCI_A0->CTLW0 |= 
-    EUSCI_A0->UCA0BRW = 0;
-    EUSCI_A0->MCTLW |= 0;
-    EUSCI_A0->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST;     //Put eUSCI in reset
+    EUSCI_A0->CTLW0 |= (BIT7);                  //Select Frame parameters and source
+    EUSCI_A0->BRW = 78;                         //Set Baud Rate
+    EUSCI_A0->MCTLW |= (BIT0 | BIT5);           //Set modulator bits
+    EUSCI_A0->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);  //Initialize eUSCI
+
+    //EUSCI_A0->UCA0IE |= (BIT0 | BIT1)  //Turn on interrupts for RX and TX
+    //NVIC_EnableIRQ(EUSCIA0_IRQn);
 }
 
-void UART_send_n(unint8_t * data, uint32_t length){
-    
+void configure_clocks(){
+    CS-> KEY = 0x695A; //Unlock module for register access
+    CS-> CTL0 = 0;     //Reset tuning parameters
+    CS-> CTL0 = (BIT(23) | CS_CTL0_DCORSEL_3);     //Setup DCO Clock
+
+    //Select ACLO = REFO, SMCLK MCLK = DCO
+    CS->CTL1 = CS_CTL1_SELA_2 | CS_CTL1_SELS_3 | CS_CTL1_SELM_3;
+    CS->KEY = 0;       //Lock CS module for register access.
+
 }
-void UART_send_byte(unint8_t data){
-    
+
+void UART_send_n(uint8_t * data, uint32_t length){
+    //Code to iterate through the transmit data
+    if(!data)
+        return;
+    volatile uint32_t n;
+
+    for(n = 0; n<length; n++){
+        UART_send_byte(data[n]);
+    }
 }
+void UART_send_byte(uint8_t data){
+    //while(~(EUSCI_A0->IFG & BIT1));  //While it hasn't finished transmitting
+    EUSCI_A0->TXBUF = data;
+}
+
+extern void EUSCIA0_IRQHandler(){
+    if (EUSCI_A0->IFG & BIT0){
+        //Recieve Stuff
+    }
+    if (EUSCI_A0->IFG & BIT1){
+        //Transmit Stuff
+    }
+}
+
