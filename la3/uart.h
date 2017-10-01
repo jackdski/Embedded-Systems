c8dbef97ea552654a5713ce4c9d6b9149117ce8a
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-10-01 11:59:18 -0600

    Update uart.h

--- la3/uart.h
+++ la3/uart.h
@@ -7,8 +7,11 @@
 #ifndef UART_H_
 #define UART_H_
 #include "msp.h"
+#include <stdint.h>
 
-void UART_send_n(unint8_t * data, uint32_t length);
-void UART_send_byte(unint8_t data);
+void configure_serial_port();
+void configure_clocks();
+void UART_send_n(uint8_t * data, uint32_t length);
+void UART_send_byte(uint8_t data);
 
 #endif
