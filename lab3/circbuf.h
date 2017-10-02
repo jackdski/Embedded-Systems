19d009bbb9bb44cfe70855f50c26a7d8ad3a1feb
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-10-01 22:29:23 -0600

    Update circbuf.h
    Tested the code, it should work now

--- lab3/circbuf.h
+++ lab3/circbuf.h
@@ -1,7 +1,28 @@
-
 /*
  * circbuf.h
  *
  *  Created on: Sep 26, 2017
  *      Author: amabo
  */
+#ifndef CIRCBUF_H_
+#define CIRCBUF_H_
+
+#include <stdint.h>
+
+typedef struct{
+    uint8_t * buffer;
+    volatile uint8_t head;
+    volatile uint8_t tail;
+    volatile uint32_t num_items;
+    volatile uint32_t length;
+}CircBuf_t;
+
+CircBuf_t * createCircBuf(uint32_t length);
+void        deleteCircBuf(CircBuf_t * buf);
+void        resetCircBuf(CircBuf_t * buf);
+int8_t      isFullCircBuf(CircBuf_t * buf);
+void        addItemCircBuf(CircBuf_t * buf, uint8_t item);
+uint8_t     removeItem(CircBuf_t * buf);
+
+
+#endif
