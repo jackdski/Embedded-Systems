87a4a4273acf654b03876b4d2a104311b21e1fac
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-10-03 09:52:44 -0600

    Update processing.c

--- lab3/processing.c
+++ lab3/processing.c
@@ -1,1 +1,26 @@
+/*
+ * processing.c
+ *
+ *  Created on: Oct 3, 2017
+ *      Author: amabo
+ */
+#include "processing.h"
+
+void configurePorts(){
+    P1->SEL0 &= ~(BIT1 | BIT4);
+    P1->SEL1 &= ~(BIT1 | BIT4);
+    P1->DIR  &= ~(BIT1 | BIT4);
+    P1->REN  |=  (BIT1 | BIT4);
+    P1->OUT  |=  (BIT1 | BIT4);
+    P1->IES  |=  (BIT1 | BIT4);
+
+    P1->IFG   = 0;
+    P1->IE   |=  (BIT1 | BIT4);
+    NVIC_EnableIRQ(PORT1_IRQn);
+}
+
+void PORT1_IRQHandler(){
+
+
+}
 
