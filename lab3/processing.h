ed0c944604ef370fc05f4fc9d43df9ef28543bc4
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-10-03 20:29:21 -0600

    Update processing.h

--- lab3/processing.h
+++ lab3/processing.h
@@ -7,24 +7,12 @@
 
 #ifndef PROCESSING_H_
 #define PROCESSING_H_
+
 #include "msp.h"
 #include <stdio.h>
 
-uint8_t alp = 0; //Number of alphabetical chars
-uint8_t pun = 0; //Number of punctuation chars
-uint8_t num = 0; //Number of numerical chars
-uint8_t whi = 0; //Number of white chars
-uint8_t ran = 0; //Number of random chars
-
 void configurePorts();
+void analyzeBuf();
 void analyzeChr(uint8_t chr);
-
-uint8_t isAlphabet(uint8_t chr);
-uint8_t isPunctuation(uint8_t chr);
-uint8_t isNumerical(uint8_t chr);
-uint8_t isWhiteSpace(uint8_t chr);
-
-
-
 
 #endif /* PROCESSING_H_ */
