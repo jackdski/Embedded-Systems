927461e9c3535bd88ca6fa04be6600965caf6b57
Jack Danielski <jackdski555@gmail.com>
2017-10-06 14:28:51 -0600

    Create conversions.h

---
+++ lab3/conversions.h
@@ -0,0 +1,15 @@
+/*
+*
+* conversions.h
+* Author: Jack Danielski and Avery Anderson
+* Date: 10-6-2017
+* 
+*/
+
+#include "msp.h"
+#include "uart.h"
+#include "circbuf.h"
+#include "processing.h"
+
+// Converts a string of numbers into an integer
+int atoi(char * a);
