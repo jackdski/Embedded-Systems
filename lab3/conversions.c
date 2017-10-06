5730a37f23325af3169b8bee8876f794fc75f49c
Jack Danielski <jackdski555@gmail.com>
2017-10-06 14:30:04 -0600

    Update and rename typeConversions.c to conversions.c

--- lab3/conversions.c
+++ lab3/conversions.c
@@ -1,3 +1,16 @@
+/*
+*
+* conversions.c
+* Author: Jack Danielski and Avery Anderson
+* Date: 10-6-2017
+* 
+*/
+
+#include "msp.h"
+#include "uart.h"
+#include "circbuf.h"
+#include "processing.h"
+
 // Converts a string of numbers into an integer
 int atoi(char * a) {
 	int returnArray [3]; 
