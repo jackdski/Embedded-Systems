941f08df56856f1e34d1e168ba1f5ad12e0a72ea
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-10-07 14:21:01 -0600

    Update processing.h

--- lab3/processing.h
+++ lab3/processing.h
@@ -14,6 +14,6 @@
 void configurePorts();
 void analyzeBuf();
 void analyzeChr(uint8_t chr);
-void itoa(uint8_t num, uint8_t * str);
+void itoa(uint32_t num, uint8_t * str);
 
 #endif /* PROCESSING_H_ */
