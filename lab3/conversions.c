e20b0f0f2f059a82d2e91d7f584e47817c566bd0
Jack Danielski <jackdski555@gmail.com>
2017-10-08 15:41:37 -0600

    Update conversions.c

--- lab3/conversions.c
+++ lab3/conversions.c
@@ -3,77 +3,78 @@
 * conversions.c
 * Author: Jack Danielski and Avery Anderson
 * Date: 10-6-2017
-* 
+*
 */
 
 #include "msp.h"
 #include "uart.h"
 #include "circbuf.h"
 #include "processing.h"
+#include "conversions.h"
 
-void itoa(uint8_t num, int size, char * str ) {
-	int i;
+#include <stdint.h>
 
-	i = size;
-	while (i >= 0) {
-		if ( i == size) {
-			str[i] = '\0';
-			i--;
-		}
-		else {
-			str[i] = num % 10;
-			str[i] += 48;
-			printf("Char %i: %c\n", i, str[i]);
-			num = num / 10;
-			i--;
-		}
-	}
+void itoa(uint16_t num, int8_t size, uint8_t * str ) {
+    int8_t i;
+    i = size;
+    while (i >= 0) {
+        if ( i == size) {
+            str[i] = '\0';
+            i--;
+        }
+        else {
+            str[i] = num % 10;
+            str[i] += 48;
+            num = num / 10;
+            i--;
+        }
+    }
 }
 
 // Converts a string of numbers into an integer
-int atoi(char * a) {
-	int returnArray [3]; 
+int16_t atoi(char * a) {
+    uint16_t returnArray [3];
 
-	int count = 0; 
-	while (count < 3) {
-		returnArray[count] = a[count] - 48;
-		count++;
-	}
+    int8_t count = 0;
+    while (count < 3) {
+        returnArray[count] = a[count] - 48;
+        count++;
+    }
 
-	int finalInt = 0;
-	finalInt = returnArray[2] + (10 * returnArray[1]) + (100 * returnArray[0]);
-	return finalInt;
+    int8_t finalInt = 0;
+    finalInt = returnArray[2] + (10 * returnArray[1]) + (100 * returnArray[0]);
+    return finalInt;
 }
 
 
-void ftoa(float number, int decimalPlace, int size, char * str) {
-	int tens = 10;
-	int i = decimalPlace - 1;
-	while (i > 0) {
-		tens = tens * 10;
-		i--;
-	}
-	number = number * (float)tens;
-	int numberInt = (int)number;
+void ftoa(float number, int8_t decimalPlace, int8_t size, uint8_t * str) {
+    uint8_t tens = 10;
+    int8_t i = decimalPlace - 1;
+    while (i > 0) {
+        tens = tens * 10;
+        i--;
+    }
+    number = number * (float)tens;
+    uint16_t numberInt = (int)number;
 
-	itoa(numberInt, size-1, str);
+    itoa(numberInt, size-1, str);
 
-	int deciPlace = decimalPlace +1;
-	char tempStr[deciPlace];
+    int8_t deciPlace = decimalPlace +1;
+    uint8_t tempStr[6];
 
-	int j = 0; 
-	while (j < deciPlace) {
-		tempStr[j] = str[deciPlace + j];
-		j++;
-	}
+    int j = 0;
+    while (j < deciPlace) {
+        tempStr[j] = str[deciPlace + j];
+        j++;
+    }
 
-	str[size - decimalPlace - 1] = '.';
+    str[size - decimalPlace - 1] = '.';
 
-	deciPlace = size - decimalPlace;
-	j = 0;
-	while (deciPlace <= size){
-		str[deciPlace] = tempStr[j];
-		deciPlace++;
-		j++;
-	}
+    deciPlace = size - decimalPlace;
+    j = 0;
+    while (deciPlace <= size){
+        str[deciPlace] = tempStr[j];
+        deciPlace++;
+        j++;
+    }
 }
