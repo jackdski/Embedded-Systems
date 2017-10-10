fe12dc26642d4b21b3f6ef2dfe965fd19595b810
Jack Danielski <jackdski555@gmail.com>
2017-10-10 00:26:04 -0600

    added timer for EC

--- lab3/main.c
+++ lab3/main.c
@@ -17,6 +17,9 @@ uint32_t pun = 0; //Number of punctuation chars
 uint32_t num = 0; //Number of numerical chars
 uint32_t whi = 0; //Number of white chars
 uint32_t ran = 0; //Number of random chars
+uint32_t wrd; //Number of words there are
+uint32_t systickCounter = 0; //Count how many time SysTick counts down
+
 
 uint8_t  currentChar = 0;
 
@@ -26,83 +29,91 @@ uint8_t  currentChar = 0;
  */
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
-	TXBuf = createCircBuf(256);
-	RXBuf = createCircBuf(256);
+    TXBuf = createCircBuf(256);
+    RXBuf = createCircBuf(256);
 
-	if(!TXBuf || !RXBuf){
-	    return;
-	}
+    if(!TXBuf || !RXBuf){
+        return;
+    }
 
-	volatile uint8_t i;
+    volatile uint8_t i;
 
 #ifdef TESTCIRCBUF
-	for(i=1; i<7; i++){
-	    addItemCircBuf(TXBuf, i);
-	}
-	uint8_t pop = removeItem(TXBuf);
-	addItemCircBuf(TXBuf, 10);
+    for(i=1; i<7; i++){
+        addItemCircBuf(TXBuf, i);
+    }
+    uint8_t pop = removeItem(TXBuf);
+    addItemCircBuf(TXBuf, 10);
 #endif
-	__enable_irq();
-	configure_clocks();
-	configure_serial_port();
-	configurePorts();
+    __enable_irq();
+    configure_clocks();
+    configure_serial_port();
+    configurePorts();
 #ifdef ESCOOT
-	configure_eScooter();
+    configure_eScooter();
 #endif
 
 
 #ifdef  UART_TESTS
-	uint8_t ptr[] = { 0xAA,'J'};
+    uint8_t ptr[] = { 0xAA,'J'};
 #endif
 
 #ifdef  BLOCKING
-	uint8_t sentence[] = "Why not Zoidberg?";
-	UART_send_n(sentence, 18);
+    uint8_t sentence[] = "Why not Zoidberg?";
+    UART_send_n(sentence, 18);
 #endif
 
 #ifdef   INTERRUPT_TEST
-	for(i =0; i < 18; i++){
-	    addItemCircBuf(TXBuf, sentence[i]);
-	}
-	UART_send_byte(removeItem(TXBuf));
+    for(i =0; i < 18; i++){
+        addItemCircBuf(TXBuf, sentence[i]);
+    }
+    UART_send_byte(removeItem(TXBuf));
+#endif
+
+#ifdef EXTRACREDIT
+    SysTick->LOAD = 1210000;   // Give SysTick a starting value to count down from
+    SysTick->CTRL = BIT0 | BIT1 | BIT2;  // Enable SysTick
 #endif
 
 while(1){
 #ifdef  UART_TESTS
-	  UART_send_n(ptr, 1);
+      UART_send_n(ptr, 1);
 #endif
 
 #ifdef DEMO
-	  if(work){
-	      work = 0;
-	      analyzeBuf();
-	  }
-	  if(transmit==1){
-	      transmit = 0;
-	      transmitRX();
-	  }
+      if(work){
+          work = 0;
+          analyzeBuf();
+      }
+      if(transmit==1){
+          transmit = 0;
+          transmitRX();
+      }
 #endif
 
 #ifdef ESCOOT
-	  if(updateDistance){
-	      updateDistance = 0;
-	      transmitDistance();
-	  }
+      if(updateDistance){
+          updateDistance = 0;
+          transmitDistance();
+      }
 
 #endif
 
 #ifdef EXTRACREDIT
-	  if(work){
-	      work = 0;
-	      analyzeChrEC(currentChar);
-	  }
-	  if(transmit){
-	      transmit = 0;
-	      transmitEC();
-	  }
+      if(work){
+          work = 0;
+          analyzeChrEC(currentChar);
+      }
+      if(transmit){
+          transmit = 0;
+          transmitEC();
+      }
+      if(SysTick->VAL == 0) {
+          systickCounter++;
+      }
 #endif
 
-	}
+    }
 }
