5b67be8237ce504f445c8221ebdd9793c88254de
Jack Danielski <jackdski555@gmail.com>
2017-10-12 09:50:43 -0600

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,5 +1,7 @@
 #include "msp.h"
+#include "adc.c"
 
+#define SCB_SCR_ENABLE_SLEEPONEXIT (0X00000002)
 
 /**
  * main.c
@@ -8,18 +10,15 @@ void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-	/*
-	 * Joystick x and y pins:
-	 *  x: J1.2     (P6.0)
-	 *  y: J3.26    (P4.4)
-	 *  sel: J1.5   (P4.1)
-	 *
-	 * Accelerometer x, y, z:
-	 *  x: J3.23    (P6.1)
-	 *  y: J3.24    (P4.0)
-	 *  z: J3.25    (P4.2)
-	 *
-	 */
+	configure_ADC();
+	__enable_interrupt();
+	SCB->SCR &= ~SCB_SCR_ENABLE_SLEEPONEXIT; //wake up on exit from ISR
 
+	while(1) {
+	    ADC14->CTL0 |= ; //Sampling and conversion start
+	    __sleep();          //blocks here until conversion finishes
+	    __no_operation();   //no-operation, good for a breakpoint
+	}
 
 }
+
