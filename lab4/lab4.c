ab01dc438c481b2013ce0ee6ac1fb5964be30a13
Jack Danielski <jackdski555@gmail.com>
2017-10-12 09:50:29 -0600

    Create lab4.c

---
+++ lab4/lab4.c
@@ -0,0 +1,77 @@
+/*
+ *
+ * lab4.c
+ * Author: Jack Danielski and Avery Anderson
+ * 10-12-17
+ *
+ */
+
+#include "msp.h"
+#include "lab4.h"
+
+
+void itoa(uint32_t num, int8_t size, uint8_t * str ) {
+    int8_t i;
+    i = size;
+    while (i >= 0) {
+        if ( i == size) {
+            str[i] = '\0';
+            i--;
+        }
+        else {
+            str[i] = num % 10;
+            str[i] += 48;
+            num = num / 10;
+            i--;
+        }
+    }
+}
+
+
+// Converts a string of numbers into an integer
+int16_t atoi(char * a) {
+    uint16_t returnArray [3];
+
+    int8_t count = 0;
+    while (count < 3) {
+        returnArray[count] = a[count] - 48;
+        count++;
+    }
+
+    int8_t finalInt = 0;
+    finalInt = returnArray[2] + (10 * returnArray[1]) + (100 * returnArray[0]);
+    return finalInt;
+}
+
+
+void ftoa(float number, uint8_t decimalPlace, uint8_t size, uint8_t * str) {
+    uint8_t tens = 10;
+    int8_t i = decimalPlace - 1;
+    while (i > 0) {
+        tens = tens * 10;
+        i--;
+    }
+    number = number * (float)tens;
+    uint16_t numberInt = (int)number;
+
+    itoa(numberInt, size-1, str);
+
+    int8_t deciPlace = decimalPlace +1;
+    uint8_t tempStr[6];
+
+    int j = 0;
+    while (j < deciPlace) {
+        tempStr[j] = str[deciPlace + j];
+        j++;
+    }
+
+    str[size - decimalPlace - 1] = '.';
+
+    deciPlace = size - decimalPlace;
+    j = 0;
+    while (deciPlace <= size){
+        str[deciPlace] = tempStr[j];
+        deciPlace++;
+        j++;
+    }
+}
