85140aef53bcbb52b13f8322712539676b68b21f
Jack Danielski <jackdski555@gmail.com>
2017-10-12 18:08:39 -0600

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,11 +1,14 @@
 #include "msp.h"
-#include "adc.c"
+#include "core_cm4.h"
+#include "adc.h"
+#include "lab4.h"
 
 #define SCB_SCR_ENABLE_SLEEPONEXIT (0X00000002)
 
 /**
  * main.c
  */
+
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
@@ -14,8 +17,10 @@ void main(void)
 	__enable_interrupt();
 	SCB->SCR &= ~SCB_SCR_ENABLE_SLEEPONEXIT; //wake up on exit from ISR
 
+	int16_t temp = 0;
 	while(1) {
-	    ADC14->CTL0 |= ; //Sampling and conversion start
+	    // get temp value in ADC14MEM0?
+	    ADC14->CTL0 |= ADC14_CTL0_SC; //Sampling and conversion start
 	    __sleep();          //blocks here until conversion finishes
 	    __no_operation();   //no-operation, good for a breakpoint
 	}
