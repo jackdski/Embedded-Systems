cf96c46f167b2e590ce233885f75b25e4460fe21
Jack Danielski <jackdski555@gmail.com>
2017-10-18 18:58:40 -0600

    Update lab4.h

--- lab4/lab4.h
+++ lab4/lab4.h
@@ -10,9 +10,13 @@
 #ifndef LAB4_H_
 #define LAB4_H_
 
-
 #include "msp.h"
 #include <stdint.h>
+
+#define CLEAR_TIMER (uint16_t)(0x0000)
+#define SET_CTL     (uint16_t)(TIMER_A_CTL_TASSEL_2 | TIMER_A_CTL_ID_3 | TIMER_A_CTL_MC__UP)
+#define COUNT_TO    (uint16_t)(37775) //Value for 5Hz is 37499 by experiment  47255 for 500ms on time
+#define SET_CCTL    (uint16_t)(0x0010)
 
 // Converts a string of numbers into an integer
 uint32_t  atoi(uint8_t * a);
