8cb5f94605b4b7fa39d8095e4a65d280874ca80f
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-10-31 08:45:53 -0600

    Add files via upload

---
+++ eScooterFinal/bluetooth.h
@@ -0,0 +1,16 @@
+/*
+ * bluetooth.h
+ *
+ *  Created on: Oct 29, 2017
+ *      Author: amabo
+ */
+
+#ifndef BLUETOOTH_H_
+#define BLUETOOTH_H_
+#include "msp.h"
+
+void configure_BLUE_UART();
+void BLUART_send_byte(uint8_t data);
+
+
+#endif /* BLUETOOTH_H_ */
