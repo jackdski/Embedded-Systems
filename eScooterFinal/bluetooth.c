bdf9180a4b87d7d4c08745ea41d9593f7ad98085
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-11-07 23:06:23 -0700

    Update bluetooth.c

--- eScooterFinal/bluetooth.c
+++ eScooterFinal/bluetooth.c
@@ -11,6 +11,8 @@
 
 extern CircBuf_t * TXBuf;
 
+//The bluetooth chip communicates at 115200 BAUD Rate
+//Set up the uart port on pins 9.6 and 9.6
 void configure_BLUE_UART(){
     //Configure UART pins, set 2-UART pins to UART mode
     P9->SEL0 |=  (BIT6 | BIT7);
@@ -27,11 +29,11 @@ void configure_BLUE_UART(){
     NVIC_EnableIRQ(EUSCIA3_IRQn);
 }
 
+//If I recieve information, just read it and throw it away.  If I want to transmit,
+//Transmit all of the information found in our circbuf
 void EUSCIA3_IRQHandler(){
     if (EUSCI_A3->IFG & BIT0){
-        //BLUART_send_byte(EUSCI_A3->RXBUF);
         uint8_t data = EUSCI_A3->RXBUF;
-        //P1->OUT ^= BIT0;
     }
 
     if (EUSCI_A3->IFG & BIT1){
@@ -45,34 +47,36 @@ void EUSCIA3_IRQHandler(){
 
 }
 
+//Send one byte of information
 void BLUART_send_byte(uint8_t data){
-//    while(!(EUSCI_A3->IFG & BIT1));  //While it hasn't finished transmitting
     EUSCI_A3->TXBUF = data;
 }
 
+//This function sends all of the data we calculate. 
 void BLUEART_SEND_DATA(float totalDistance, float spd,uint8_t direction){
     uint8_t distanceS[7];
     uint8_t speedS[7];
-
-    //ftoa(totalDistance,2,7,distanceS);
-    //ftoa(spd,2,7,speedS);
+    
+    //Turn our floats into strings
     ftoa(totalDistance,distanceS,3);
     ftoa(spd,speedS,3);
 
+    //Send the distance
     loadToBuf(TXBuf, "You have traveled ", 18);
     loadToBuf(TXBuf, distanceS, 7);
     loadToBuf(TXBuf, "cm", 2);
     addItemCircBuf(TXBuf, 0x0D);
     addItemCircBuf(TXBuf, 0x0A);
 
-
+    //Send the speed
     loadToBuf(TXBuf, "Your speed is ", 14);
     loadToBuf(TXBuf, speedS, 7);
     loadToBuf(TXBuf, "cm/s", 4);
     addItemCircBuf(TXBuf, 0x0D);
     addItemCircBuf(TXBuf, 0x0A);
 
-
+    //If I am not moving, say I am stopped, otherwise use the boolean
+    //direction value to say which direction you are travelling.
     if((uint8_t)spd == 0){
         loadToBuf(TXBuf, "You're Stopped", 15);
     }
@@ -88,10 +92,11 @@ void BLUEART_SEND_DATA(float totalDistance, float spd,uint8_t direction){
     addItemCircBuf(TXBuf, 0x0D);
     addItemCircBuf(TXBuf, 0x0A);
 
-
+    //Start transmissions and block until they are completed.
     EUSCI_A3->IFG |= BIT1;
     while(!isEmpty(TXBuf));
 
+    //Re-enable interrupts
     NVIC_EnableIRQ(PORT1_IRQn);
     NVIC_EnableIRQ(ADC14_IRQn); // Enable ADC interrupt in NVIC module
     NVIC_EnableIRQ(PORT3_IRQn);
