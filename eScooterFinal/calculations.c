2851397f6f5aecf6f8e6fe1d38e2e4ed18f8e200
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-11-07 23:08:42 -0700

    Update calculations.c

--- eScooterFinal/calculations.c
+++ eScooterFinal/calculations.c
@@ -15,6 +15,9 @@ extern volatile float totalDistance;
 extern volatile uint8_t direction;
 uint8_t lastSpeed = 0;
 
+//Speed is calcualted by dividing the distance since the last measurement by .1s
+//This works as the function is called every .1 seconds. We also update our total distance
+//in this function.
 float speed(){
     float spd = 02.25114 * beamBreaks / 0.1;
     totalDistance += beamBreaks*02.25114;
@@ -22,6 +25,9 @@ float speed(){
     return spd;
 }
 
+//We use a lastSpeed variable to determine direction. If we were stopped, and are no longer,
+//We then can assume we are now moving in the direction we just accelerated. The VYNADC value
+//needs to be calibrated to account for the MSP's angle in the box.
 uint8_t calculateDirection(float spd){
     if(lastSpeed > 0 || spd  == 0){
         lastSpeed = spd;
