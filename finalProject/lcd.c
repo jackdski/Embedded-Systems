dff30bd4f291352b250737d76e6893be0224c22d
Jack Danielski <jackdski555@gmail.com>
2017-11-23 12:26:32 -0700

    Create lcd.c

---
+++ finalProject/lcd.c
@@ -0,0 +1,18 @@
+/*
+ * BoosterPack Header Connection - Pin Function
+ * J1.7     -   LCD SPI clock
+ * J2.13    -   LCD SPI chip select
+ * J2.15    -   LCD SPI MOSI
+ * J4.31    -   LCD reset pin
+ * J4.39    -   LCD backlight
+ *
+ */
+
+#include "msp.h"
+#include "lcd.h"
+#include "stdint.h"
+
+void defaultScreen() {
+
+
+}
