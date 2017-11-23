cd2c8e5555e681ac68d53755e9f808ede638e7e5
Jack Danielski <jackdski555@gmail.com>
2017-11-23 12:26:49 -0700

    Create lcd.h

---
+++ finalProject/lcd.h
@@ -0,0 +1,17 @@
+/*
+*
+* Wireless Bike Lock
+* lcd.h
+*
+* 11-12-17
+*
+*/
+
+#ifndef LCD_H_
+#define LCD_H_
+
+//#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+void defaultScreen();
+
+#endif
