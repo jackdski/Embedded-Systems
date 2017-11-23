1efc532703f3deebae506b24d87e32f7c930c864
Jack Danielski <jackdski555@gmail.com>
2017-11-23 12:29:15 -0700

    Create buttons.h

---
+++ finalProject/buttons.h
@@ -0,0 +1,20 @@
+/*
+*
+* Wireless Bike Lock
+* buttons.h
+*
+* 11-20-17
+*
+*/
+
+#ifndef BUTTONS_H_
+#define BUTTONS_H_
+
+#include "msp.h"
+#include <stdint.h>
+
+void configButtons();
+
+void configLED();
+
+#endif
