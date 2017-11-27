9b507a3f00f59ef8ab0657c12bbbd8983a5d6c7f
Jack Danielski <jackdski555@gmail.com>
2017-11-26 17:38:09 -0700

    Update time.h

--- finalProject/lock/time.h
+++ finalProject/lock/time.h
@@ -11,8 +11,13 @@
 #ifndef TIME_H_
 #define TIME_H_
 
+// make the systick timer start counting
 void startSystick();
+
+// get hours and mins from station -- needs work!
 void getTimeBluetooth();
+
+// calculate the amount of ticks needed for the time to be checked out
 uint32_t checkoutTimerTicks(uint8_t hours, uint8_t mins);
 
 #endif
