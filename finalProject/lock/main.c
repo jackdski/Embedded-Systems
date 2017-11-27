edd155cb96d739c7aff215f5085093fef805d05d
Jack Danielski <jackdski555@gmail.com>
2017-11-26 17:26:20 -0700

    Update main.c

--- finalProject/lock/main.c
+++ finalProject/lock/main.c
@@ -1,20 +1,43 @@
+/*
+*
+* Wireless Bike Lock - Lock
+* main.c
+*
+* 11-26-17
+*
+*/
+
 #include "msp.h"
 #include "Buzzer.h"
 #include "LockButton.h"
+#include "time.h"
+#include "bluetooth.h"
+#include <stdint.h>
 
-/**
- * main.c
- */
+uint32_t systickCounter = 0;
+uint32_t checkoutTimerTicksVal = 0;
+uint32_t overtime = 0;
+uint8_t hours = 0;
+uint8_t mins = 0;
+
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
-	configure_Buzzer();
-	configure_LockButton();
+    checkoutTimerTickVal = checkoutTimerTicks(hours, mins); // calculate time to count to
 
-	P1->DIR |= BIT0;
+    // configs
+    configure_Buzzer();
+    configure_LockButton();
+    configSystick();
 
-	while(1){
+    // if time has been set use this
+    //startSysTick();
 
-	}
+
+    P1->DIR |= BIT0;
+
+    while(1){
+
+    }
 }
