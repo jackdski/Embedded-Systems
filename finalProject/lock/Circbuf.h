5a459d2626d6ad6c1047ec7ba3219120b2db09dc
Jack Danielski <jackdski555@gmail.com>
2017-12-02 13:24:05 -0700

    Create Circbuf.h

---
+++ finalProject/lock/Circbuf.h
@@ -0,0 +1,32 @@
+/*
+ * Wireless Bikelock - Lock
+ * circbuf.h
+ *
+ * Created on: Sep 26, 2017
+ */
+#ifndef CIRCBUF_H_
+#define CIRCBUF_H_
+
+#include <stdint.h>
+
+typedef struct{
+    uint8_t * buffer;
+    volatile uint8_t * head;
+    volatile uint8_t * tail;
+    volatile uint32_t num_items;
+    volatile uint32_t length;
+}CircBuf_t;
+
+CircBuf_t * createCircBuf(uint32_t length);
+void        deleteCircBuf(CircBuf_t * buf);
+void        resetCircBuf(CircBuf_t * buf);
+int8_t      isFullCircBuf(CircBuf_t * buf);
+void        addItemCircBuf(CircBuf_t * buf, uint8_t item);
+uint8_t     isEmpty(CircBuf_t * buf);
+void        loadToBuf(CircBuf_t * buf, uint8_t * string, uint8_t length);
+uint8_t     removeItem(CircBuf_t * buf);
+
+int16_t stoi(char * a);
+
+
+#endif
