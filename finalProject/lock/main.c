381b9057f43c937576deb56ba0d797109276d711
Jack Danielski <jackdski555@gmail.com>
2017-12-02 09:20:42 -0700

    Update main.c

--- finalProject/lock/main.c
+++ finalProject/lock/main.c
@@ -4,20 +4,32 @@
 #include "Bluetooth.h"
 #include "SystemClock.h"
 #include "RGB.h"
+#include "Time.h
 
-/**
- * main.c
- */
+
+uint32_t systickCounter = 0;        //counts how man 0.5s have passed
+uint32_t checkoutTimerTicksVal = 0; //how many ticks the bike is being taken out for
+uint32_t overtime = 0;  // how many ticks over the above time the bike is out
+uint8_t hours = 0;      // how many hours the bike is out for
+uint8_t mins = 0;       // how many mins the bike is out for
+
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
+	checkoutTimerTickVal = checkoutTimerTicks(hours, mins); // calculate time to count to
+	
+	//configs
 	configure_SystemClock();
 	configure_Buzzer();
 	configure_LockButton();
 	configure_Bluetooth();
 	configure_RGB();
+	configureSystick();
 
+	// if time has been set use this
+    	//startSysTick();
+	
 	P1->DIR |= BIT0;
 	P5->OUT |= BIT5;
 	P6->DIR |= BIT0 | BIT1;
