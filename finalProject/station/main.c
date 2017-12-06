8cdca172e953e6890291b701fe46732554312616
Jack Danielski <jackdski555@gmail.com>
2017-12-05 22:14:32 -0700

    Update main.c

--- finalProject/station/main.c
+++ finalProject/station/main.c
@@ -1,33 +1,83 @@
 /*
-*
 * Wireless Bike Lock
 * main.c
 *
 * 11-12-17
-*
 */
-#include "msp.h"
 
-#include "bluetooth.h"
-#include "buttons.h"
-#include "lcd.h"
-#include "register.h"
+#include "msp.h"
+#include "Bluetooth.h"
+#include "Borrow.h"
+#include "Buttons.h"
+#include "Lcd.h"
+//#include "Register.h"
+#include "Circbuf.h"
+
+
+/*
+ * Wireless Bike Lock Bluetooth Packet Structure
+ *
+ * [ DIRECTION | CMDTYPE | CMDDATA | CHKSUM  ]
+ * [  8 bits   | 8 bits  | 128 bits| 24 bits ]
+ *                  168 bits per message
+ *                  21 bytes per message
+ */
+// Direction stuff
+//#define STOL (0X00)
+//#define LTOS (0x11)
+
+// Command types so receiver knows what to do with this data
+#define HEARTBEAT   (0x01) // ack request with dummy data
+#define RFIDDATA    (0X02) // contains rfid data to give to lock
+#define DELETERFID  (0X03) // delete the rfid data currently stored
+#define CHKSUMVALID (0x04) // the checksum is correct
+#define CHKSUMBAD   (0x05) // the checksum is incorrect
+#define RETURNHB    (0x06) // ack return with dummy data
+#define RETURNRFID  (0x07) // says it got RFID data correctly
+
+volatile CircBuf_t * TXBuf;
+volatile CircBuf_t * RXBuf;
+
+volatile uint8_t checkPacket = 0;
+
+//List_t * userList;
 
 void main(void) {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
     // configs
-    configButtons();
+    //configButtons();
+    configure_SystemClock();
     configLED();
-    configure_BLUE_UART();
+    configure_Bluetooth();
+
+    // User List
+    //userList = createList(300);
+    TXBuf = createCircBuf(21);
+    RXBuf = createCircBuf(21);
 
     P1->SEL0 &= ~(BIT0);
     P1->SEL1 &= ~(BIT0);
     P1->DIR |= BIT0;
     P1->OUT |= BIT0;
 
+    volatile uint8_t i;
+    volatile uint8_t j;
+    BTCMD parsed;
+    uint8_t check;
+
     while(1) {
-        P1->OUT ^= BIT0;
-        volatile int i;
-        for (i = 0; i < 30000; i++);
+        // If buffer is not being filled check it
+        if(!(EUSCI_A2->IFG |= BIT1)) {
+            parsed = parseBTCMD();
+            check = checksum(parsed);
+            if(check){
+                //do stuff with data
+
+            }
+            else {
+                sendCheckSumBad();
+            }
+        }
+        check = 0;
     }
 }
