0efbc7cd6a814fb3a054d93e8c9e3c02c6c98282
stefansuar <34014325+stefansuar@users.noreply.github.com>
2017-12-07 09:32:17 -0700

    Create clock.h

---
+++ finalProject/lcd/clock.h
@@ -0,0 +1,25 @@
+/*
+ * clock.h
+ *
+ *  Created on: Dec 7, 2017
+ *      Author: Stefan
+ */
+
+#ifndef CLOCK_H_
+#define CLOCK_H_
+
+
+#include "msp.h"
+
+
+void configure_SystemClock();
+
+void Delay0();
+void Delay1();
+void Delay2();
+void Delay3();
+void Delay4();
+void Delay5();
+
+
+#endif /* CLOCK_H_ */
