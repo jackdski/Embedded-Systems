014bb8d2cee50be00f191dd53438c61917d0472f
Jack Danielski <jackdski555@gmail.com>
2017-12-08 21:15:00 -0700

    Update main.c

--- finalProject/station/main.c
+++ finalProject/station/main.c
@@ -15,17 +15,33 @@
 /*
  * Wireless Bike Lock Bluetooth Packet Structure
  *
- * [    RFIDDATA    ]
- * [    16 BYTES    ]
- *              152 BITS
- *              19 BYTES
+ * [    MSGTYPE     |   RFIDDATA    ]
+ * [    1 BYTE      |   16 BYTES    ]
+ *              136 BITS
+ *              17 BYTES
  */
-
+/*
 volatile CircBuf_t * TXBuf;
 volatile CircBuf_t * RXBuf;
+volatile CircBuf_t * RFIDBuf;
+*/
 
-volatile uint8_t dummyRfidCheck;
-volatile uint8_t dummyTimeCheck;
+CircBuf_t * TXBuf;
+CircBuf_t * RXBuf;
+CircBuf_t * RFIDBuf;
+
+volatile uint8_t hbCheck = 0; // if this starts with H
+
+/*
+ * 0 = main screen
+ * 1 = student ID has been entered
+ * 2 = RFID tag data has been entered
+ */
+volatile uint8_t registerStage = 0;
+
+// 1 if the station gets a HB message back
+// 0 otherwise
+volatile uint8_t receivedHB = 0;
 
 void main(void) {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
@@ -38,11 +54,11 @@ void main(void) {
 
     // User List
     //userList = createList(300);
-    TXBuf = createCircBuf(200);
-    RXBuf = createCircBuf(200);
+    TXBuf = createCircBuf(17);
+    RXBuf = createCircBuf(17);
+    RFIDBuf = createCircBuf(16);
 
-    uint8_t * dummyRFID = "XXXXXXXXXXXXXXXX";
-    uint8_t * dummyTime = "XXX";
+    uint8_t heartbeat[17] = "HXXXXXXXXXXXXXXXX";
 
     P1->SEL0 &= ~(BIT0);
     P1->SEL1 &= ~(BIT0);
@@ -51,30 +67,74 @@ void main(void) {
 
     volatile uint8_t i;
     volatile uint8_t j;
-    uint8_t * rfidData; // [16] = "0000000000000000";
-    uint8_t * time; // [3] = "000";
+    volatile uint8_t rfidData[17] = "00000000000000000";
+
+    loadToBuf(RXBuf,"HXXXXXXXXXXXXXXXX",17);
+    uint8_t test[16];
+    test[0]  = 0x02;
+    test[1]  = 0x31;
+    test[2]  = 0x38;
+    test[3]  = 0x30;
+    test[4]  = 0x30;
+    test[5]  = 0x38;
+    test[6]  = 0x33;
+    test[7]  = 0x43;
+    test[8]  = 0x44;
+    test[9]  = 0x34;
+    test[10] = 0x31;
+    test[11] = 0x31;
+    test[12] = 0x37;
+    test[13] = 0x0D;
+    test[14] = 0x0A;
+    test[15] = 0x03;
+    for(j=0;j<16;j++) {
+        addItemCircBuf(RFIDBuf, test[j]);
+    }
 
     while(1) {
-        // If buffer is not being filled check it
-        if(!(EUSCI_A2->IFG |= BIT1)) {
-            for(i=0;i<16;i++) {
+        // If RX buffer is fill check what is in it
+        if(isFullCircBuf(RXBuf)) {
+            // load rfidData with what is in RXBuf
+            for(i=0;i<17;i++) {
                 rfidData[i] = RXBuf->buffer[i];
             }
-            for(i=16;i<19;i++){
-                time[i-16] = RXBuf->buffer[i];
-            }
-            dummyRfidCheck = 0;
-            dummyTimeCheck = 0;
-            for(i=0;i<16;i++){
-                if(dummyRFID[i] == rfidData[i]) {
-                    dummyRfidCheck++;
+            // count how many characters it has in common with the heartbeat msg
+            for(i=0;i<17;i++){
+                if(heartbeat[i] == rfidData[i]) {
+                    hbCheck++;
                 }
             }
-            for(i=16;i<19;i++) {
-                if(dummyTime[i] == time[i]) {
-                    dummyTimeCheck++;
+            // if station receives heartbeat msg back, send RFID data
+            if((heartbeat[0] == 'H') && (hbCheck == 17)) {
+                /*
+                loadToBuf(TXBuf, "HXXXXXXXXXXXXXXXX", 17);
+                while(!(isEmpty(TXBuf))) {
+                    sendByte(removeItem(TXBuf));
+                }
+                */
+                while(!(isEmpty(RFIDBuf))){
+                    sendByte(removeItem(RFIDBuf));
                 }
             }
+            else{
+                uint8_t * rfidString;
+                for(j=1;j<17;j++){
+                    rfidString[j-1] = RXBuf->buffer[j];
+                }
+                loadToBuf(RFIDBuf, rfidString, 16);
+                //EUSCI_A2->IFG |= BIT1;
+                sendByte(removeItem(TXBuf));
+            }
+            hbCheck = 0;
+        }
+        // registerStage == 2 initiates the station to send rfid data
+        if((registerStage == 2) && (isFullCircBuf(RXBuf))) {
+            if(receivedHB) {
+                // assume RFIDBuf is already loaded
+                EUSCI_A2->IFG |= BIT1;
+                sendByte(removeItem(RFIDBuf));
+            }
+            registerStage == 0; // reset and return to main screen/loop
         }
     }
 }
