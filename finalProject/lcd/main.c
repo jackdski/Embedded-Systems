80861091952eb0274265f3e39e09d54d0e57b9cd
stefansuar <34014325+stefansuar@users.noreply.github.com>
2017-12-10 15:02:18 -0700

    Update main.c

--- finalProject/lcd/main.c
+++ finalProject/lcd/main.c
@@ -9,15 +9,20 @@
 #include <grlib.h>
 #include "Crystalfontz128x128_ST7735.h"
 #include <stdio.h>
+#include <stdint.h>
 #include <arm_math.h>
 #include "arm_const_structs.h"
 #include "ButtonConfig.h"
 #include "display.h"
 #include "clock.h"
 
-
+volatile uint32_t test = 1234;
+volatile uint32_t SID = 0;
 extern volatile uint8_t slideNum;
 Graphics_Context g_sContext;
+uint8_t result;
+char string[4];
+
 
 int main(void)
 {
@@ -26,6 +31,10 @@ int main(void)
     GPIO_configure();
     LCD_configure();
     __enable_irq();
+
+    setID();
+    result = itoa(SID, string, 3, 10);
+
 
     while(1)
     {
