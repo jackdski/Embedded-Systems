8f78488f4dd55409d7273bbfcbe5b7b419f4e123
Jack Danielski <jackdski555@gmail.com>
2017-12-10 19:53:35 -0700

    Update Bluetooth.c

--- finalProject/station/Bluetooth.c
+++ finalProject/station/Bluetooth.c
@@ -11,6 +11,8 @@
 
 extern CircBuf_t * TXBuf;
 extern CircBuf_t * RXBuf;
+extern CircBuf_t * TESTtxBuf;
+extern CircBuf_t * TESTrxBuf;
 
 void configure_SystemClock(){
     CS-> KEY = 0x695A; //Unlock module for register access
@@ -23,47 +25,116 @@ void configure_SystemClock(){
 }
 
 //Connect 3.3 to the RX
-void configure_Bluetooth(){
+void configure_serial_port(){
     //Configure UART pins, set 2-UART pins to UART mode
     P3->SEL0 |=  (BIT2 | BIT3);
     P3->SEL1 &= ~(BIT2 | BIT3);
 
     EUSCI_A2->CTLW0 |= EUSCI_A_CTLW0_SWRST;     //Put eUSCI in reset
     EUSCI_A2->CTLW0 |= (BIT7);                  //Select Frame parameters and source
-    EUSCI_A2->BRW = 78;                          //Set Baud Rate
+    EUSCI_A2->BRW = 78;                         //Set Baud Rate
     EUSCI_A2->MCTLW |= (BIT0 | BIT5);           //Set modulator bits
     EUSCI_A2->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);  //Initialize eUSCI
 
-    EUSCI_A2->IFG &= ~(BIT0 | BIT1);
+    EUSCI_A2->IFG &= ~(BIT1 | BIT0);
     UCA2IE |= (BIT0 | BIT1);  //Turn on interrupts for RX and TX
     NVIC_EnableIRQ(EUSCIA2_IRQn);
 }
 
-inline void sendByte(uint8_t data){
-    EUSCI_A2->TXBUF = data;
-    while(!(EUSCI_A2->IFG & BIT1));
+void configAltBT(){
+    //Configure UART pins, set 2-UART pins to UART mode
+    P1->SEL0 |=  (BIT2 | BIT3);
+    P1->SEL1 &= ~(BIT2 | BIT3);
+
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST;     //Put eUSCI in reset
+    EUSCI_A0->CTLW0 |= (BIT7);                  //Select Frame parameters and source
+    EUSCI_A0->BRW = 78;                         //Set Baud Rate
+    EUSCI_A0->MCTLW |= (BIT0 | BIT5);           //Set modulator bits
+    EUSCI_A0->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);  //Initialize eUSCI
+
+    EUSCI_A0->IFG &= ~(BIT1 | BIT0);
+    UCA0IE |= (BIT0 | BIT1);  //Turn on interrupts for RX and TX
+    NVIC_EnableIRQ(EUSCIA0_IRQn);
 }
 
-void bluetooth_send_n(uint8_t * data, uint8_t length){
+inline void UART_send_byte(uint8_t data){
+    EUSCI_A2->TXBUF = data;
+}
+
+inline void UART_send_bytez(uint8_t data){
+    EUSCI_A0->TXBUF = data;
+}
+
+void UART_send_n(uint8_t * data, uint8_t length){
     //Code to iterate through the transmit data
     if(!data)
         return;
     volatile uint8_t n;
+
     for(n = 0; n<length; n++){
-        sendByte(data[n]);
+        UART_send_byte(data[n]);
     }
 }
 
+
+void EUSCIA0_IRQHandler(){
+//echo
+    /*
+    if (EUSCI_A0->IFG & BIT0){
+        //addItemCircBuf(TXBuf, EUSCI_A0->RXBUF);
+        addItemCircBuf(TESTrxBuf, EUSCI_A0->RXBUF);
+        //if(TXBuf->num_items == 1){
+         //   EUSCI_A0->IFG |= BIT1;
+        //}
+    }
+    */
+    if(EUSCI_A0->IFG & BIT0) {
+        P1->OUT |= BIT0;
+        addItemCircBuf(RXBuf,EUSCI_A0->RXBUF);
+    }
+    if(EUSCI_A0->IFG & BIT1){
+        //Transmit Stuff
+        if(isEmpty(TXBuf)){
+            EUSCI_A0->IFG &= ~BIT1;
+            return;
+        }
+        //UART_send_bytez(removeItem(TESTtxBuf));
+        UART_send_bytez(removeItem(TXBuf));
+    }
+}
+
+// use echo stuff to return data to station
 void EUSCIA2_IRQHandler(){
-    if (EUSCI_A2->IFG & BIT0){
+/*
+    if(EUSCI_A2->IFG & BIT0) {
+        //addItemCircBuf(TXBuf, EUSCI_A2->RXBUF);
+        addItemCircBuf(RXBuf, EUSCI_A2->RXBUF);
+        //if(TXBuf->num_items == 1){
+          //  EUSCI_A2->IFG |= BIT1;
+        //}
         addItemCircBuf(RXBuf, EUSCI_A2->RXBUF);
     }
     if (EUSCI_A2->IFG & BIT1){
         //Transmit Stuff
-        if(isEmpty(TXBuf)) {
+        if(isEmpty(TXBuf)){
             EUSCI_A2->IFG &= ~BIT1;
             return;
         }
-        sendByte(removeItem(TXBuf));
+        UART_send_byte(removeItem(TXBuf));
+    }*/
+    //echo
+
+    if(EUSCI_A2->IFG & BIT0) {
+        P1->OUT |= BIT0;
+        addItemCircBuf(RXBuf,EUSCI_A2->RXBUF);
+    }
+    if (EUSCI_A2->IFG & BIT1){
+        //Transmit Stuff
+        if(isEmpty(TXBuf)){
+            EUSCI_A2->IFG &= ~BIT1;
+            return;
+        }
+        //UART_send_bytez(removeItem(TESTtxBuf));
+        UART_send_byte(removeItem(TXBuf));
     }
 }
