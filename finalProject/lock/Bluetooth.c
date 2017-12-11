d66da566ff5b951f1c6b22b51826934e8a2d9c2d
Jack Danielski <jackdski555@gmail.com>
2017-12-11 00:02:49 -0700

    Update Bluetooth.c

--- finalProject/lock/Bluetooth.c
+++ finalProject/lock/Bluetooth.c
@@ -21,34 +21,25 @@ void configure_Bluetooth(){
 
     EUSCI_A3->CTLW0 |= EUSCI_A_CTLW0_SWRST;     //Put eUSCI in reset
     EUSCI_A3->CTLW0 |= (BIT7);                  //Select Frame parameters and source
-    EUSCI_A3->BRW = 78;                          //Set Baud Rate
+    EUSCI_A3->BRW = 78;                         //Set Baud Rate
     EUSCI_A3->MCTLW |= (BIT0 | BIT5);           //Set modulator bits
     EUSCI_A3->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);  //Initialize eUSCI
 
-    EUSCI_A3->IFG &= ~(BIT1 | BIT0);
+    //EUSCI_A3->IFG &= ~(BIT6 | BIT7);
+    //UCA3IE |= (BIT6 | BIT7);  //Turn on interrupts for RX and TX
+    EUSCI_A3->IFG &= ~(BIT0 | BIT1);
     UCA3IE |= (BIT0 | BIT1);  //Turn on interrupts for RX and TX
+
     NVIC_EnableIRQ(EUSCIA3_IRQn);
 }
 
 inline void sendByte(uint8_t data){
     EUSCI_A3->TXBUF = data;
-    while(!(EUSCI_A2->IFG & BIT1));
 }
 
-void bluetooth_send_n(uint8_t * data, uint8_t length){
-    //Code to iterate through the transmit data
-    if(!data)
-        return;
-    volatile uint8_t n;
-
-    for(n = 0; n<length; n++){
-        sendByte(data[n]);
-    }
-}
 
 void EUSCIA3_IRQHandler(){
     if (EUSCI_A3->IFG & BIT0){
-        //P1->OUT ^= BIT0;
         addItemCircBuf(RXBuf, EUSCI_A3->RXBUF);
     }
     if (EUSCI_A3->IFG & BIT1){
@@ -57,6 +48,8 @@ void EUSCIA3_IRQHandler(){
             EUSCI_A3->IFG &= ~BIT1;
             return;
         }
-        sendByte(removeItem(TXBuf));
+        //sendByte(removeItem(TXBuf));
+        EUSCI_A3->TXBUF = removeItem(TXBuf);
+
     }
 }
