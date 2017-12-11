cefca2de48da188b12b9a62d99991d7673fd69fc
Jack Danielski <jackdski555@gmail.com>
2017-12-10 21:56:16 -0700

    Update Bluetooth.h

--- finalProject/station/Bluetooth.h
+++ finalProject/station/Bluetooth.h
@@ -1,13 +1,22 @@
 /*
+ * Wireless Bike Lock - Station
  * Bluetooth.h
  *
- *  Created on: Dec 6, 2017
- *      Author: amabo
  */
 
 #ifndef BLUETOOTH_H_
 #define BLUETOOTH_H_
 
+#include "msp.h"
+#include "Circbuf.h"
 
+void configure_SystemClock();
+void configure_Systick();
+void configure_serial_port();
+void configAltBT();
+void UART_send_byte(uint8_t data);
+void UART_send_bytez(uint8_t data);
+void UART_send_n(uint8_t * data, uint8_t length);
+void send_registration(Bike_t bikeList) {
 
-#endif /* BLUETOOTH_H_ */
+#endif
