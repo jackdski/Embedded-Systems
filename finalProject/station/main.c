7e1336b034eccd96c587089aebad5c1beb58cb6a
AveryAnderson <30330527+AveryAnderson@users.noreply.github.com>
2017-12-16 10:26:24 -0700

    Update main.c

--- finalProject/station/main.c
+++ finalProject/station/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 
+//Include all of our relevant files
 #include "msp.h"
 #include "Student.h"
 #include "Circbuf.h"
@@ -13,23 +14,24 @@
 #include "Joystick.h"
 #include "Bluetooth.h"
 
-
+//Create the heads of our linked lists
 volatile Student_t * registry;
 volatile Bike_t    * bikeList;
 
+//Create the RFID circular buffer and new RFID flag
 volatile CircBuf_t * RFIDBuf;
 volatile uint8_t     newRFID = 0;
 
+//Create the Logging circular buffer and send log flag
 volatile CircBuf_t * LOGBuf;
 volatile uint8_t LOGFlag;
 
+//Create circular buffers to control the transmission and reception of information between the station and lock
 volatile CircBuf_t * RXBuf;
 volatile CircBuf_t * TXBuf;
-
-
-
 volatile uint8_t  transmitFlag = 0;
 
+//Create the State variable
 volatile State stationState;
 /**
  * main.c
@@ -41,6 +43,7 @@ void main(void)
 	//Initialize the registry to have no students
 	registry = NULL;
 
+	//Configure all of our peripherals
 	configure_RFID();
 	configure_SystemClock();
 	configure_Buttons();
@@ -50,22 +53,23 @@ void main(void)
 	configure_Bluetooth();
 	configure_Systick();
 
-
-
+	//Add our bike to the bike list with SN: 1234
 	makeBike(1234);
 
+	//Set the direction of an onboard LED for debug purposes
 	P1->DIR |= BIT0;
 
-
-	//Create an RFIDBuffer to hold our 16 chars of RFID data
+	//Create an our circular buffers
 	RFIDBuf = createCircBuf(16);
-	LOGBuf  = createCircBuf(600);
+	LOGBuf  = createCircBuf(256);
 	RXBuf   = createCircBuf(17);
 	TXBuf   = createCircBuf(17);
 
+	//Make the initial state of the system be the Standby state
 	enterState(Standby);
 
 	while(1){
+	    //If we want to transmit meaning we have a new user or a minute has passed
 	    if(transmitFlag && stationState == Standby){
 	        transmitFlag = 0;
 	        //loop through all of the bikes we have
@@ -98,12 +102,14 @@ void main(void)
 	                    myBike->inUse = 0;
 	                }
 	            }
+		    //If the bike is overtime and needs a warning
 	            else if(myBike->inUse == 3){
 	                //If I get a response, I am ready to deregister the bike at the next transmission
 	                if(send_Warning()){
 	                    myBike->inUse = 4;
 	                }
 	            }
+		    //If the bike is overtime and is ready to be deregistered
 	            else if(myBike->inUse == 4){
 	                //If I get a response, the bike is now deregistered. Reset bike and student
 	                if(send_RFID("0000000000000000")){
@@ -115,6 +121,7 @@ void main(void)
 	            myBike = myBike->nextBike;
 	        }
 	    }
+   	    //If the station receives RFID data begin progressing through the RFID recived logic
 	    if(newRFID){
 	        newRFID = 0;
 
@@ -127,17 +134,18 @@ void main(void)
 	            readRFID[i] = removeItem(RFIDBuf);
 	        }
 
-
+	    //Attempt to find the student we are working with
             Student_t * activeStudent = findStudent(readRFID);
 
-	        //Check to see if we have already registered this student and enter the appropriate state
+	        //If we didn't find the student, register this new RFID card. Enter Register state
 	        if(!activeStudent){
 	            enterState(Register);
 	            activeStudent = registerStudent(readRFID);
 	        }
 
-	        //If we successfully registered the new student proceed to checkout
+	        //If we successfully registered, or had already found a student, proceed to checkout
 	        if(activeStudent){
+		    //Don't let users check out more than one bike
 	            if(activeStudent->HASBIKE){
 	                enterState(DoubleDipping);
 	            }
@@ -146,13 +154,17 @@ void main(void)
 	                checkOutBike(activeStudent);
 	            }
 	        }
-
+		
+		//After we have updated the bike information, return to standby
 	        enterState(Standby);
 
 
 	    }
+	    
+	    //Constantly poll to see if the bikes need to be deregistered based off of the RTC's last updates 
 	    flagDeregister();
 
+	    //If we had a log button interrupt, send the log
 	    if(LOGFlag){
 	        LOGFlag = 0;
 	        sendLog();
